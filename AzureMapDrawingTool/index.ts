2cf73f85919d3b23dd27677a91e13b154755bdb4
chris v <cvitacek@hotmail.com>
2025-02-14 06:34:59 -0600

    changes to ControlManifest to support Lat/Lon.

--- AzureMapDrawingTool/index.ts
+++ AzureMapDrawingTool/index.ts
@@ -1,52 +1,82 @@
 import { IInputs, IOutputs } from "./generated/ManifestTypes";
+import * as atlas from "azure-maps-control";
+import * as atlasDrawing from "azure-maps-drawing-tools";
 
 export class AzureMapDrawingTool implements ComponentFramework.StandardControl<IInputs, IOutputs> {
-    /**
-     * Empty constructor.
-     */
+    
+/* #region Globals */
+
+    //Component Framework...
+    private _context: ComponentFramework.Context<IInputs>;
+    private _container: HTMLDivElement;
+    private _notifyOutputChanged: () => void;
+
+    //Atlas Map...
+    private _map: atlas.Map;
+    //private _drawingManager: atlasDrawing.drawing.DrawingManager;
+
+    //Data Source...
+    //private _dataSource: atlas.source.DataSource;
+    //private _setData: EventListenerOrEventListenerObject;
+    //private _dataBounds: atlas.data.BoundingBox;
+
+    //Data Elements...
+    //private _latitude: number;
+    //private _longitude: number;
+    //private _geoJson: string
+    //private _zoom: number;
+
+    //HTML...
+    private _mapContainer: HTMLDivElement;
+    //private _eventPanel: HTMLDivElement;
+    private _inputElement: HTMLInputElement;
+
+    /* #endregion */
+
     constructor() {
         // Empty
     }
 
-    /**
-     * Used to initialize the control instance. Controls can kick off remote server calls and other initialization actions here.
-     * Data-set values are not initialized here, use updateView.
-     * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to property names defined in the manifest, as well as utility functions.
-     * @param notifyOutputChanged A callback method to alert the framework that the control has new outputs ready to be retrieved asynchronously.
-     * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
-     * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
-     */
+    
     public init(
         context: ComponentFramework.Context<IInputs>,
         notifyOutputChanged: () => void,
         state: ComponentFramework.Dictionary,
         container: HTMLDivElement
     ): void {
-        // Add control initialization code
+        try { 
+            console.log("init");
+        }
+        catch (error) {
+            console.log(error);
+        }
     }
 
-
-    /**
-     * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
-     * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
-     */
     public updateView(context: ComponentFramework.Context<IInputs>): void {
-        // Add code to update control view
+        try { 
+            console.log("updateView");            
+        }
+        catch (error) {
+            console.log(error);
+        }
     }
 
-    /**
-     * It is called by the framework prior to a control receiving new data.
-     * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as "bound" or "output"
-     */
     public getOutputs(): IOutputs {
+        try { 
+            console.log("getOutputs");            
+        }
+        catch (error) {
+            console.log(error);
+        }
         return {};
     }
 
-    /**
-     * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
-     * i.e. cancelling any pending remote calls, removing listeners, etc.
-     */
     public destroy(): void {
-        // Add code to cleanup control if necessary
+        try { 
+            console.log("destroy");            
+        }
+        catch (error) {
+            console.log(error);
+        }
     }
 }
